557b31a2526b4396716ff7894a9f86289958715c
Fathan Maulana <94539481+fathanmaul@users.noreply.github.com>
2025-01-23 16:16:54 +0700

    add svelte-seo integration for improved SEO and metadata management

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,11 +5,33 @@
 	import Footer from '$lib/components/Footer.svelte';
 	import Headline from '$lib/components/Headline.svelte';
 	import Project from '$lib/components/Project.svelte';
+	import SvelteSeo from 'svelte-seo';
 </script>
+
+<SvelteSeo 
+	title="Fathan Maulana"
+	description="Fathan Maulana's portfolio created using SvelteKit"
+	canonical="https://www.fathanmaul.my.id"
+	keywords="Fathan Maulana, Portfolio, Svelte, Web Developer, Web Project"
+	openGraph={{ 
+		title: "Fathan Maulana",
+		description: "Fathan Maulana's portfolio created using SvelteKit",
+		images: [
+			{
+				alt: "github profile picture",
+				url: "https://avatars.githubusercontent.com/u/94539481?v=4",
+				type: "image",
+				secure_url: "https://avatars.githubusercontent.com/u/94539481?v=4"
+			}
+		],
+		url: "https://www.fathanmaul.my.id",
+		type: "website",
+	}}
+/>
 
 <svelte:head>
 	<title>Fathan Maulana</title>
-	<meta name="description" content="Svelte Portfolio" />
+	
 </svelte:head>
 
 <section>
@@ -22,6 +44,3 @@
 	</div>
 	<Footer />
 </section>
-
-<style>
-</style>
