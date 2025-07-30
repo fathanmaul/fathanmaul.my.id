e73a00fe6c421524e17ea90a73959d95f5595b76
Fathan Maulana <94539481+fathanmaul@users.noreply.github.com>
2025-07-30 15:50:37 +0700

    fix(intro) : updating intro text

--- src/components/molecules/IntroText.tsx
+++ src/components/molecules/IntroText.tsx
@@ -8,13 +8,7 @@ export default function IntroText() {
         Fathan Maulana
       </h1>
       <p className="text-justify text-sm lg:text-xl font-extralight 2xl:mt-[24px]" id="hero-paragraph">
-        I'm a Software Engineer based in Bondowoso, East Java, with a passion
-        for Front-End Development. I'm a detail-oriented and dedicated
-        individual who enjoys turning ideas into functional and visually
-        appealing web experiences. As a fresh graduate, I'm just getting started
-        in the freelance world. I've worked with various front-end frameworks
-        and love building things for the web—be it simple websites or
-        interactive applications.
+        I'm a Software Engineer based in Bondowoso, East Java, with a passion for Front-End Development. I enjoys turning ideas into functional and visually appealing web experiences. I've worked with various front-end frameworks and love building things for the web—be it simple websites or interactive applications.
       </p>
     </div>
   );
